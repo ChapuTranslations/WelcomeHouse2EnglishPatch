dda5fb301f8283dc8243c8ae009d51382b2dcf3a
ChapuTranslations <chaputranslations@gmail.com>
2024-12-04 16:37:24 -0300

    removed comments from patcher.py

--- patcher.py
+++ patcher.py
@@ -43,13 +43,3 @@ if __name__ == '__main__':
 	for image in IMAGES:
 		subprocess.run('java -jar tools/jpsxdec.jar -x bin/wh2.idx -i {} -replacetim pics/{}'.format(image[0], image[1]), shell = True)
 	print('=============== SHOULD BE FINISHED ===============')
-
-# java -jar tools/jpsxdec.jar -f bin/Welcome\ House\ 2\ -\ Keaton\ and\ His\ Uncle\ \(Japan\)\ \(Track\ 1\).bin -x bin/wh2.idx
-# java -jar tools/jpsxdec.jar -x bin/wh2.idx -i 1 -dir exe/
-# mv exe/SLPS_006.33 exe/src_SLPS_006.33
-# wine tools/armips.exe main\.asm
-# mv bin/Welcome\ House\ 2\ -\ Keaton\ and\ His\ Uncle\ \(Japan\).cue bin/Welcome\ House\ 2\ -\ Keaton\ and\ His\ Uncle\ \(Japan\)\ \(Track\ 1\).cue
-# wine tools/psxinject.exe -v bin/Welcome\ House\ 2\ -\ Keaton\ and\ His\ Uncle\ \(Japan\)\ \(Track\ 1\)\.bin SLPS_006\.33 exe/SLPS_006\.33
-# java -jar tools/jpsxdec.jar -x bin/wh2.idx -i {} -replacetim {}
-
-
